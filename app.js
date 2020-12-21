a69b060331d4056e056bed7b360b1b93bcbe34ef
apurba_debnath <apurba871@gmail.com>
2020-12-21 21:39:50 +0530

    Added few more routes

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.get("/", function(req, res) { //req & res are objects
 	res.send("Hi there, I am here!");
 });
 
+// "/bye" : "Goodbye!"
+
+app.get("/bye", function(req, res) {
+	res.send("Goodbye!");
+});
+
+// "/meooow" : "Greetings master"
+app.get("/meooow", function(req, res) {
+	res.send("Hello meooow25, long time no see...");
+});
+
 app.listen(3000, function() {
 	console.log("Server started, listening app on port 3000...");
 });
