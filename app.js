90c7c1938f744968f90bd91fa284babd93fed271
apurba_debnath <apurba871@gmail.com>
2020-12-21 21:23:34 +0530

    Made first route

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 var express = require("express");
 var app = express();
+
+//Creating routes...
+
+// "/" : "Hi there, I am here!"
+
+app.get("/", function(req, res) { //req & res are objects
+	res.send("Hi there, I am here!");
+});
+
+app.listen(3000, function() {
+	console.log("Server started, listening app on port 3000...");
+});
